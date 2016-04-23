2668b2fe3b04c2bcb5f133800c6122f843b9b47e
jojodee <joan@projectcodex.co>
2016-04-23 16:41:48 +0200

    trying out single web page dev

---
+++ practise.js
@@ -0,0 +1,21 @@
+// Imports react library for acess to components
+import React from 'react';
+//Imports ReactDOM to enable the render component
+import ReactDOM 'react-dom';
+
+//Creatae react Component
+exports default class practise extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
